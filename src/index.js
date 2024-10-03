8788816752c22c9fa89567ec3e209ea625b3022c
Bilal Hamza <131806480+BilalHamza7@users.noreply.github.com>
2024-10-03 10:36:04 +0530

    latest commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
-import Home from './features/home';
+import Homepage from './features/home/homepage';
 import './index.css';
+import Layout from './features/layout';
+import Profile from './features/profile/profile';
+import About from './features/home/about';
+import Contact from './features/home/contact';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"> {/** element={<Layout />} */}
-          <Route index element={<Home />} /> 
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
+          <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        fredoka: ['Fredoka', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
